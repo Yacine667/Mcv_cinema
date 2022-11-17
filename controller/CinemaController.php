efe7f2c5cff8d80c01ba2b69a37e5777300afc44
Yacine667 <sid.yacinee@gmail.com>
2022-11-17 13:51:39 +0100

    Exo_Mcv_Cinema

---
+++ controller/CinemaController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Controller;
+use Model\Connect;
+
+class CinemaController {
+
+    /**
+     * Lister les films
+     */
+    public function listFilms() {
+
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->query("
+            SELECT titre, annee_sortie_fr FROM film
+        ");
+
+        require "view/listFilms.php";
+    }
+}
